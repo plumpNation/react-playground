38a2f6bb013b6ba790582873d50abec89196ec12
Gavin King <gavin.king@acando.no>
2018-08-12 15:50:56 +0200

    Added example of flatpickr consumption with single language

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 // @flow
 
-import React, { Component } from 'react'
-import './App.css'
-
+import Flatpickr from 'react-flatpickr'
 import MyComponent from './components/MyComponent'
 import FunctionAsChildren from './components/FunctionAsChildren'
+import React, { Component } from 'react'
+
+import 'flatpickr/dist/themes/material_green.css'
+import { Norwegian } from 'flatpickr/dist/l10n/no'
+import './App.css'
 
 type AppProps = {}
 type AppState = {
-  name: string
+  name: string,
+  date: Date
 }
 
 class App extends Component<AppProps, AppState> {
   state = {
-    name: 'Jeff'
+    name: 'Jeff',
+    date: new Date()
   }
 
   handleClick = () => {
@@ -32,6 +37,10 @@ class App extends Component<AppProps, AppState> {
         <FunctionAsChildren name={this.state.name} onClick={this.handleClick}>
           {({name, onClick}) => <button onClick={onClick}>{name}</button>}
         </FunctionAsChildren>
+
+        <Flatpickr options={{locale: Norwegian}} data-enable-time
+          value={this.state.date}
+          onChange={date => { this.setState({ date }) }} />
       </div>
     )
   }
