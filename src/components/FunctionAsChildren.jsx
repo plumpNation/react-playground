9e078a7139f15b3c67894d894cacc6c4a536f7d2
Gavin King <gavin.king@acando.no>
2018-08-12 14:24:13 +0200

    Documentation around the function as children is lacking

--- src/components/FunctionAsChildren.jsx
+++ src/components/FunctionAsChildren.jsx
@@ -2,7 +2,10 @@
 
 /**
  * This example shows how you can accept a prop and then run that prop
- * back into the transcluded children
+ * back into the transcluded children.
+ *
+ * Interesting article arguing that this technique is an anti-pattern:
+ * @see https://americanexpress.io/faccs-are-an-antipattern/
  */
 
 import React from 'react'
